b110d0a3c8166ba88468e5d4e6073e536e82363d
Fomagran <fomagran6@gmail.com>
2022-07-07 21:07:38 -0500

    Added Cell Configuration -  UITableViewCell with UIHostingConfiguration

---
+++ WWDC/2022/UseSwiftUIWithUIKit/HeartRateTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeartRateTableViewCell.swift
+//  WWDC
+//
+//  Created by Fomagran on 2022/07/07.
+//
+
+import UIKit
+
+class HeartRateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- WWDC/2022/UseSwiftUIWithUIKit/HeartRateViewController.swift
+++ WWDC/2022/UseSwiftUIWithUIKit/HeartRateViewController.swift
@@ -11,15 +11,16 @@ import SwiftUI
 @available(iOS 16.0, *)
 class HeartRateViewController: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        self.configure()
+//        self.configure()
     }
-
     
     private func configure() {
         let data:HeartData = HeartData(beatsPerMinute: 10)
@@ -27,5 +28,22 @@ class HeartRateViewController: UIViewController {
         hostingController.sizingOptions = .preferredContentSize
         hostingController.modalPresentationStyle = .popover
         self.present(hostingController, animated: true)
+    }
+}
+
+@available(iOS 16.0, *)
+extension HeartRateViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "HeartRateTableViewCell") as! HeartRateTableViewCell
+        cell.contentConfiguration = UIHostingConfiguration {
+            Label("Heart Rate", systemImage: "heart.fill")
+                .foregroundColor(.pink)
+                .font(.system(.subheadline, weight: .bold))
+        }
+        return cell
     }
 }
